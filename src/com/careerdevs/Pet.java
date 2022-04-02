bbd16d4c5392f6f1a16845b4b1d1bca7649c10db
JohnQuezada <johnaquezada@gmail.com>
2022-04-02 17:13:16 -0400

    Pet Store Pet Class update

--- src/com/careerdevs/Pet.java
+++ src/com/careerdevs/Pet.java
@@ -1,5 +1,43 @@
 package com.careerdevs;
 
 public class Pet {
-    
+
+    public String species;
+    public int numberOfLegs;
+    public boolean isLandAnimal;
+
+    public Pet(String species, int numberOfLegs, boolean isLandAnimal) {
+        this.species = species;
+        this.numberOfLegs = numberOfLegs;
+        this.isLandAnimal = isLandAnimal;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public int getNumberOfLegs() {
+        return numberOfLegs;
+    }
+
+    public boolean isLandAnimal() {
+        return isLandAnimal;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public void setNumberOfLegs(int numberOfLegs) {
+        this.numberOfLegs = numberOfLegs;
+    }
+
+    public void setLandAnimal(boolean isLandAnimal) {
+        this.isLandAnimal = isLandAnimal;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{ " + "Species: " + species + ", " + "Number of Legs: " + numberOfLegs + ", " + "Is this pet a land animal: " + isLandAnimal;
+    }
 }
